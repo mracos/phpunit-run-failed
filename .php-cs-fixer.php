f2e5f02a5baa1b07cd049b187b42d2643d68bbb8
marcos ferreira <marcosp4f@gmail.com>
2025-10-01 11:45:35 -0300

    Add configuration for phpstan and php-cs-fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests')
+    ->in(__DIR__ . '/examples')
+    ->name('*.php');
+
+$config = new PhpCsFixer\Config();
+
+return $config
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        '@PHP80Migration' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'binary_operator_spaces' => ['default' => 'single_space'],
+        'blank_line_after_opening_tag' => true,
+        'cast_spaces' => ['space' => 'single'],
+        'concat_space' => ['spacing' => 'one'],
+        'declare_strict_types' => true,
+        'fully_qualified_strict_types' => true,
+        'function_typehint_space' => true,
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
+        'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
+        'no_empty_phpdoc' => true,
+        'no_extra_blank_lines' => ['tokens' => ['extra']],
+        'no_superfluous_phpdoc_tags' => ['allow_mixed' => true],
+        'no_unused_imports' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'phpdoc_align' => ['align' => 'left'],
+        'phpdoc_order' => true,
+        'phpdoc_trim' => true,
+        'return_type_declaration' => ['space_before' => 'none'],
+        'single_quote' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arrays', 'arguments', 'parameters']],
+        'trim_array_spaces' => true,
+        'visibility_required' => ['elements' => ['method', 'property']],
+        'whitespace_after_comma_in_array' => true,
+    ])
+    ->setFinder($finder);
